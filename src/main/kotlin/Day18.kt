8da86e01b599205ecdd36f8a8c809fe42f7917d7
Niklas <derNiklaas@users.noreply.github.com>
2024-12-18 08:41:16 +0100

    Add Day18

---
+++ src/main/kotlin/Day18.kt
@@ -0,0 +1,78 @@
+import java.util.PriorityQueue
+import kotlin.math.min
+import utils.AoCDay
+import utils.Vec2D
+import utils.splitAndMapToInt
+
+class Day18 : AoCDay() {
+    val positions = input.map {
+        val (x, y) = it.splitAndMapToInt(",")
+        Vec2D(x, y)
+    }
+
+    val amountFirstPart = min(positions.size, 1024)
+    val dim = if (amountFirstPart == 1024) 70 else 6
+    val start = Vec2D(0, 0)
+    val end = Vec2D(dim, dim)
+
+    override fun part1(): Any {
+        val fallen = mutableSetOf<Vec2D>()
+        positions.take(amountFirstPart).forEach { fallen += it }
+
+        fun search(): Int {
+            val queue = PriorityQueue<Pair<Vec2D, Int>>(compareBy { it.second })
+            queue += start to 0
+            val visited = mutableSetOf<Vec2D>()
+            while (queue.isNotEmpty()) {
+                val (current, steps) = queue.poll()
+                if (current in visited) continue
+                if (current == end) return steps
+                visited += current
+                for (neighbour in current.getNeighbours(true)) {
+                    if (neighbour !in fallen && neighbour.x in 0..dim && neighbour.y in 0..dim) {
+                        queue += neighbour to steps + 1
+                    }
+                }
+            }
+            error("no path")
+        }
+
+        return search()
+    }
+
+    override fun part2(): Any {
+        fun check(n: Int): Boolean {
+            val blocks = positions.take(n).toSet()
+            val queue = PriorityQueue<Pair<Vec2D, Int>>(compareBy { it.second })
+            queue += start to 0
+            val visited = mutableSetOf<Vec2D>()
+            while (queue.isNotEmpty()) {
+                val (current, steps) = queue.poll()
+                if (current in visited) continue
+                if (current == end) return true
+                visited += current
+                for (neighbour in current.getNeighbours(true)) {
+                    if (neighbour !in blocks && neighbour.x in 0..dim && neighbour.y in 0..dim) {
+                        queue += neighbour to steps + 1
+                    }
+                }
+            }
+            return false
+        }
+
+        tailrec fun binarySearch(low: Int, high: Int): Int {
+            val n = (low + high) / 2
+            return if (n == low) n
+            else if (check(n)) binarySearch(n, high)
+            else binarySearch(low, n)
+        }
+
+        val limit = binarySearch(1024, positions.size)
+        val pos = positions.drop(limit).first()
+        return "${pos.x},${pos.y}"
+    }
+}
+
+fun main() {
+    Day18().execute()
+}
