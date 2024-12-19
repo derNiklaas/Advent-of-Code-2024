059941ca175069bf4d28f20e16c87237bf9445df
Niklas <derNiklaas@users.noreply.github.com>
2024-12-19 12:01:31 +0100

    Add Day19

---
+++ src/main/kotlin/Day19.kt
@@ -0,0 +1,40 @@
+import utils.AoCDay
+import utils.chunkedInput
+
+class Day19 : AoCDay() {
+    val chunks = input.chunkedInput()
+    val patterns = chunks[0].first().split(", ")
+    val towels = chunks[1]
+    val matchCounts = towels.map { countMatches(it, patterns) }
+
+    override fun part1(): Any {
+        return towels.indices.count { matchCounts[it] > 0 }
+    }
+
+    override fun part2(): Any {
+        return matchCounts.sum()
+    }
+
+    fun countMatches(design: String, patterns: List<String>): Long {
+        return dp(design, patterns)
+    }
+
+    fun dp(design: String, patterns: List<String>): Long {
+        val cache = LongArray(design.length + 1) { 0L }
+        cache[0] = 1L
+        for (index in design.indices) {
+            val currentCount = cache[index]
+            for (pattern in patterns) {
+                if (design.substring(index).startsWith(pattern)) {
+                    cache[index + pattern.length] += currentCount
+                }
+            }
+        }
+        return cache[design.length]
+    }
+
+}
+
+fun main() {
+    Day19().execute()
+}
